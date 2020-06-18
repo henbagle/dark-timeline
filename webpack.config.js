5d2df604969796d94e12e38fe52624134e325008
Ben Hagle <ben.hagle@gmail.com>
2020-06-18 14:12:07 -0400

    Added bootstrap and sass to webpack, rather than from cdn

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,9 @@ const webpack = require("webpack");
 module.exports = {
     entry: {
         eventEditor:"./src/eventEditor.js",
-        timeline:"./src/timeline.js"
+        timeline:"./src/timeline.js",
+        index: "./src/index.js",
+        style: "./src/style.scss"
     },
     module: {
         rules: [
@@ -14,11 +16,30 @@ module.exports = {
             use: {
               loader: "babel-loader"
             }
-          }
+          },
+          {
+            test: /\.(scss)$/,
+            use: [{
+              loader: 'style-loader', // inject CSS to page
+            }, {
+              loader: 'css-loader', // translates CSS into CommonJS modules
+            }, {
+              loader: 'postcss-loader', // Run postcss actions
+              options: {
+                plugins: function () { // postcss plugins, can be exported to postcss.config.js
+                  return [
+                    require('autoprefixer')
+                  ];
+                }
+              }
+            }, {
+              loader: 'sass-loader' // compiles Sass to CSS
+            }]
+          },
         ]
     },
     resolve: {
-        extensions: ['*', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx', '.scss']
     },
     output: {
         path: path.join(__dirname, "public/dist"),
